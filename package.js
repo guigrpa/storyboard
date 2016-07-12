87c34f15995631e145147ca60ee248729d69716e
G. Grau <guigrpa@gmail.com>
2016-07-12 23:19:46 +0200

    v2.0.0-alpha.3

--- package.js
+++ package.js
@@ -4,7 +4,7 @@
 // Basic config
 // ===============================================
 const NAME = 'storyboard';
-const VERSION = '2.0.0-alpha.2';
+const VERSION = '2.0.0-alpha.3';
 const DESCRIPTION = 'End-to-end, hierarchical, real-time, colorful logs and stories';
 const KEYWORDS = ['log', 'logging', 'websockets', 'console', 'isomorphic'];
 
