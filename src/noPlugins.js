272697e8a9d5174281078d4491ebb3eca8acdc1c
G. Grau <guigrpa@gmail.com>
2016-07-17 15:34:42 +0200

    Minor

--- src/noPlugins.js
+++ src/noPlugins.js
@@ -6,10 +6,9 @@ export default storyboard;
 export * from './storyboard';
 
 // Show deprecation notice
-const { chalk } = storyboard;
+const { mainStory } = storyboard;
 /* eslint-disable no-console */
-console.log(chalk.yellow.bold(
-  "Use of 'storyboard/lib/noPlugins' is deprecated in Storyboard 2.0.0"));
-console.log(chalk.yellow.bold(
-  "Import 'storyboard' directly (it no longer enables any plugin by default)"));
+mainStory.warn('storyboard', "Use of 'storyboard/lib/noPlugins' is deprecated in Storyboard 2.0.0");
+mainStory.warn('storyboard',
+  "Import 'storyboard' directly (it no longer enables any plugin by default)");
 /* eslint-enable no-console */
