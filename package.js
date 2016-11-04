978f693fe91ad3815b6fe1649fbc55236d6d232d
G. Grau <guigrpa@gmail.com>
2016-11-04 17:11:07 +0100

    Bump jest-html, correcting some snapshots

--- package.js
+++ package.js
@@ -254,7 +254,7 @@ const specs = {
 
     // Testing with Jest
     'jest': '16.1.0-alpha.691b0e22',
-    'jest-html': '^1.0.0',
+    'jest-html': '^1.0.1',
     'react-test-renderer': '15.3.2',
     'babel-jest': '16.0.0',
 
