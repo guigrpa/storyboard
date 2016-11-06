b4b05211f141f2464d11512b6fd59afb3421d1de
G. Grau <guigrpa@gmail.com>
2016-11-06 06:40:09 +0100

    Improve snapshots

--- testJest/chromeExtension/components/mockComponent.js
+++ testJest/chromeExtension/components/mockComponent.js
@@ -1,5 +1,10 @@
+/* eslint-disable react/prop-types */
+
 const React = require('react');
 
 module.exports = (name) => (props) => (
-  <div dataMockType={name} {...props}>{name}</div>
+  <div dataMockType={name} {...props}>
+    <div style={{ fontWeight: 'bold', color: 'blue' }}>{name}</div>
+    {props.children}
+  </div>
 );
