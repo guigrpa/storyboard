500dcc1c8ae1d5146cdbaf5009d8a2a9c48c6424
G. Grau <guigrpa@gmail.com>
2016-11-08 16:27:35 +0100

    Bump jest-html

--- package.js
+++ package.js
@@ -256,7 +256,7 @@ const specs = {
 
     // Testing with Jest
     'jest': '16.1.0-alpha.691b0e22',
-    'jest-html': '^1.1.0',
+    'jest-html': '^1.2.0',
     'react-test-renderer': '15.3.2',
     'babel-jest': '16.0.0',
 
